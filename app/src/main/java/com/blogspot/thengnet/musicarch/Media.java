51b65f8d48594e0f7096773588b89a01f3bcf4bb
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-04-09 21:49:12 +0100

    Set up Media POJO class

---
+++ app/src/main/java/com/blogspot/thengnet/musicarch/Media.java
@@ -0,0 +1,20 @@
+package com.blogspot.thengnet.musicarch;
+
+public class Media {
+    private String mediaTitle;
+    private String mediaLength; // otherwise long -- when Time conversion is set up
+
+    public Media (String mediaTitle, String mediaLength) {
+        this.mediaTitle = mediaTitle;
+        this.mediaLength = mediaLength;
+    }
+
+    public String getMediaTitle () {
+        return this.mediaTitle;
+    }
+
+    public String getMediaLength () {
+        return this.mediaLength;
+    }
+
+}
