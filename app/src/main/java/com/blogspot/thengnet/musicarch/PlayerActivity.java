40811f0b607959e9a3605a8f267f7a17d8e1a748
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-04-09 23:32:22 +0100

    Set up PlayerActivity to: - get title & length from intent and set content to appropriate {@link TextView}s - set elapsed time to "00:00"

--- app/src/main/java/com/blogspot/thengnet/musicarch/PlayerActivity.java
+++ app/src/main/java/com/blogspot/thengnet/musicarch/PlayerActivity.java
@@ -1,13 +1,33 @@
 package com.blogspot.thengnet.musicarch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class PlayerActivity extends AppCompatActivity {
+
+    private static String title, length;
+    private TextView tvMediaTitle, tvMediaElapsed, tvMediaLength;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        Intent media = getIntent();
+        if (media != null) {
+            title = media.getStringExtra("media-title");
+            length = media.getStringExtra("media-length");
+
+            tvMediaTitle = (TextView) findViewById(R.id.tv_media_title);
+            tvMediaLength = (TextView) findViewById(R.id.tv_total_time);
+            tvMediaTitle.setText(title);
+            tvMediaTitle.append(" playing");
+            tvMediaLength.setText(length);
+
+            tvMediaElapsed = (TextView) findViewById(R.id.tv_elapsed_time);
+            tvMediaElapsed.setText("00:00");
+        }
     }
 }
